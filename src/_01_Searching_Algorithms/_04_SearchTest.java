918f690b460e4dbe9d58ce22f52a5531c9b4eede
League Student <league@iMac-9.attlocal.net>
2019-11-18 16:55:30 -0800

    Search Method, More and More tests, started Searching and Sorting Algorithms

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -10,24 +10,50 @@ class _04_SearchTest {
 	
 	@Test
 	public void testLinearSearch() {
+		//_00_LinearSearch
 		//1. use the assertEquals method to test your linear search method.
+		String[] test1 = {"hi", "yeet", "something", "matee"};
+		String[] test2 = {"SCR", "350", "331", "68"};
+		String[] test3 = {"GCR", "2019", "wen", "???"};
+		
+		assertEquals(1, _00_LinearSearch.linearSearch(test1, "yeet"));
+		assertEquals(3, _00_LinearSearch.linearSearch(test2, "68"));
+		assertEquals(0, _00_LinearSearch.linearSearch(test3, "GCR"));
 	}
 
 	@Test
 	public void testBinarySearch() {
 		//2. use the assertEquals method to test your binary search method.
 		//   remember that the array must be sorted
+		int[] test1 = {0, 1, 2, 3};
+		int[] test2 = {4, 5, 6, 7};
+		int[] test3 = {68, 331, 350, 707};
+		assertEquals(2, _01_BinarySearch.binarySearch(test1, 0, test1.length-1, 2));
+		assertEquals(0, _01_BinarySearch.binarySearch(test2, 0, test2.length-1, 4));
+		assertEquals(1, _01_BinarySearch.binarySearch(test3, 0, test3.length-1, 331));
 	}
 	
 	@Test
 	public void testInterpolationSearch() {
 		//3. use the assertEquals method to test your interpolation search method.
 		//   remember that the array must be sorted and evenly distributed
+		int[] test1 = {0, 1, 2, 3};
+		int[] test2 = {4, 5, 6, 7};
+		int[] test3 = {68, 331, 350, 707};
+		assertEquals(2, _02_InterpolationSearch.interpolationSearch(test1, 2));
+		assertEquals(0, _02_InterpolationSearch.interpolationSearch(test2, 4));
+		assertEquals(1, _02_InterpolationSearch.interpolationSearch(test3, 331));
 	}
 	
 	@Test
 	public void testExponentialSearch() {
 		//4. use the assertEquals method to test your exponential search method.
 		//   remember that the array must be sorted
+		int[] test1 = {0, 1, 2, 3};
+		int[] test2 = {4, 5, 6, 7};
+		int[] test3 = {68, 331, 350, 707};
+		assertEquals(2, _03_ExponentialSearch.exponentialSearch(test1, 2));
+		assertEquals(0, _03_ExponentialSearch.exponentialSearch(test2, 4));
+		assertEquals(1, _03_ExponentialSearch.exponentialSearch(test3, 331));
 	}
 }
--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -4,6 +4,46 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
-		return 0;
+		for(int i=0; i<eggs.size(); i++) {
+			if(eggs.get(i).equals("cracked")) {
+				return i;
+			}
+		}
+		return -1;
+	}
+	public static int countPearls(List<Boolean> list) {
+		int number = 0;
+		for(int i=0; i<list.size(); i++) {
+			if(list.get(i).equals(true)) {
+				number++;
+			}
+		}
+		return number;
+	}
+	public static Double findTallest(List<Double> peeps) {
+		Double tallest = 0.0;
+		for(int i=0; i<peeps.size(); i++) {
+			if(peeps.get(i)>tallest) {
+				tallest=peeps.get(i);
+			}
+		}
+		return tallest;
+	}
+	public static String findLongestWord(List<String> words) {
+		String currentLongest = "";
+		for(int i=0; i<words.size(); i++) {
+			if(words.get(i).length()>currentLongest.length()) {
+				currentLongest=words.get(i);
+			}
+		}
+		return currentLongest;
+	}
+	public static boolean containsSOS(List<String> code) {
+		for(int i=0; i<code.size(); i++) {
+			if(code.get(i).equals(" ... --- ... ")) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
